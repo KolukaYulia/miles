41cddef3b2732ef7668e7f1415f5c58570c82db2
“Koluka” <koluka78@mail.ru>
2023-01-30 12:55:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int tiket = 90;
+        int mile = 20;
+        System.out.println("Количество начисленных миль:");
+        System.out.println(tiket / mile);
+
+          }
+}
