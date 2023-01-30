148364e02e12d3e8e765ff29e12b18cbea7fec1f
“Koluka” <koluka78@mail.ru>
2023-01-30 18:05:07 +0300

    Add Class

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        int cost;
+        cost = price / 20;
+        return cost;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int tiket = 90;
-        int mile = 20;
-        System.out.println("Количество начисленных миль:");
-        System.out.println(tiket / mile);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
 
-          }
+    }
 }
